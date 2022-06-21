81f4c669496690df4c735246175330296aef0a8b
PavelBujko <briz@tut.by>
2022-06-21 23:36:44 +0300

    add

---
+++ script.js
@@ -0,0 +1,32 @@
+const buttonFi= document.querySelector('#first');
+const buttonSe= document.querySelector('#second');
+const buttonTh= document.querySelector('#third');
+
+const contentFi= document.querySelector('#first-content');
+const contentSe= document.querySelector('#second-content');
+const contentTh= document.querySelector('#third-content');
+
+const tabContent= document.querySelectorAll('.tabcontent');
+const buttons = document.querySelectorAll('button');
+
+console.log(tabContent);
+console.log(buttons);
+
+
+function openTab(id){
+    for (let item of id){
+        if (item != event.target){
+            item.classList.remove('active');
+        } 
+    }
+}
+
+for(let i = 0; i < 3; i++) {
+    buttons[i].addEventListener('click', (event) => {
+        openTab(tabContent)
+        openTab(buttons)
+
+        buttons[i].classList.add('active')
+        tabContent[i].classList.add('active')
+    });
+}
